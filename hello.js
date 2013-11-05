9216d65dde85414e79bc440c8b0588431997972b
Joel Martin <github@martintribe.org>
2013-11-04 22:30:22 -0600

    hello.js: add print out of stats.

--- hello.js
+++ hello.js
@@ -2,9 +2,12 @@
  * Created by Kanthanarasimhaiah on 29/10/13.
  */
 
-var bst=require("./bst");
+var binarytree = require("./binarytree"),
+    bst = require("./bst");
 
 var tree = new bst.BST();
 tree.insert(10,11,12);
 console.log(tree.tuple());
 console.log(tree.links());
+
+console.log("STATS:", binarytree.STATS);
