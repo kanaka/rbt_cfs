4f35849b4937eead1d91fe714608d619bf78877c
deepthi <diewdeep@gmail.com>
2013-11-15 01:39:25 -0600

    added queue and sorted starting time

--- readFile.js
+++ readFile.js
@@ -6,7 +6,7 @@ fs = require('fs');
 var num_of_tasks;
 var total_time;
 
-fs.readFile('/US_Univ/Fall2013/algo_project-clone/cse5311-project/data/simple3.txt', 'utf8', function (err,data) {
+fs.readFile('/US_Univ/Fall2013/algo_project-clone/cse5311-project/data/mixed6.txt', 'utf8', function (err,data) {
     if (err) {
         return console.log(err);
     }
@@ -17,25 +17,22 @@ fs.readFile('/US_Univ/Fall2013/algo_project-clone/cse5311-project/data/simple3.t
     total_time=input[0].substr(2);
     console.log(num_of_tasks,total_time);//,task_one );
 
-    //sort
-//    input.forEach(function (){
-//        //console.log("hi");
-//        var array1=input[1].split(' ');
-//        console.log(array1.sort());
-//    });
-    var timeArray='';
-    for(var j=1;j<input.length;j++){
-        //console.log(input.length);
-        var timeDiffArray=input[j].split(' ');
-        timeArray=timeArray.concat(timeDiffArray[2]);
-        //console.log(timeArray.sort(function(a,b) {return a-b}));
-        console.log(timeArray);
-    }
+    //fetch starting time of all tasks and storing in timeArray
+    var queue= [];
+//    var testqueue=[];
 
-    //process each task based on time of each task
+    for(var j=1;j<input.length;j++){
+        var timeDiffArray=input[j].split(' ');
+        queue.push(timeDiffArray[1]);
+//        testqueue.push(input[j]);
+    }
+    console.log(queue);
+    console.log(queue.sort());
+
+        //process each task based on starting time of each task
     for(var i=0;i<total_time;i++)
     {
-        //console.log(i);
+
 
     }
 });
