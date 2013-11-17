61fcfbcc011d93f87f0840d47645ccb4b2eaa773
Joel Martin <github@martintribe.org>
2013-11-17 11:14:54 -0500

    Full task data and sorting fixes.
    - Parse numeric parts of task (start_time and duration) as numbers.
    - Define task as a map (with id, start_time, duration).
    - Numeric sort the task queue by start_time attribute.
    - Cleanup debug output.

--- readFile.js
+++ readFile.js
@@ -11,31 +11,30 @@ fs.readFile(fileName, 'utf8', function (err,data) {
     if (err) {
         return console.log(err);
     }
-    console.log(data);
+    //console.log(data);
 
     var input=data.split('\n');
     var line1 = input[0].split(' ');
     num_of_tasks=parseInt(line1[0], 10);
     total_time=parseInt(line1[1], 10);
-    console.log(num_of_tasks,total_time);//,task_one );
+    console.log("Numer of Tasks:", num_of_tasks);
+    console.log("Total Time:", total_time);
 
-    //fetch starting time of all tasks and storing in timeArray
+    // read in the tasks
     var queue= [];
-//    var testqueue=[];
 
-    for(var j=1;j<num_of_tasks;j++){
-        var timeDiffArray=input[j].split(' ');
-        queue.push(timeDiffArray[1]);
-//        testqueue.push(input[j]);
+    for(var j=1;j<num_of_tasks;j++) {
+        var task_data=input[j].split(' ');
+        var task = {id         :          task_data[0],
+                    start_time : parseInt(task_data[1], 10),
+                    duration   : parseInt(task_data[2], 10)};
+        queue.push(task);
     }
+    function sort_function (a, b) {
+        return a.start_time - b.start_time;
+    }
+    queue.sort(sort_function);
+    console.log("Tasks:");
     console.log(queue);
-    console.log(queue.sort());
-
-        //process each task based on starting time of each task
-    for(var i=0;i<total_time;i++)
-    {
-
-
-    }
 });
 
